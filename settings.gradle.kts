3081633af15dc1f186664e45a2e9559b8519ea0c
Prive <tezov.app@gmail.com>
2023-08-03 21:07:50 +0200

    update catalog plugin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,7 +34,7 @@ pluginManagement {
 buildscript {
 
     dependencies {
-        classpath("com.tezov:plugin_project:1.0.0")
+        classpath("com.tezov:plugin_project:1.0.1")
         classpath("com.android.tools.build:gradle:8.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
     }
